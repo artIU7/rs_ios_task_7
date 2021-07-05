6fb9d8e1e7f78a1bc3bc0d6c38f3d30bdb9dbcbb
brazilec22 <stratienko.artem@gmail.com>
2021-07-05 19:16:38 +0300

    Task7 : Add UILabel [Title] without 'SF pro'

--- task7/ViewController.m
+++ task7/ViewController.m
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UILabel *RSSchoolLabel;
 @end
 
 @implementation ViewController
@@ -16,5 +16,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // call :: addRSSchoolLabel
+    [ self addRSSchoolLabel ];
+}
+- ( void ) addRSSchoolLabel  {
+    CGRect labelFrame = CGRectMake(self.view.center.x/2,
+                                   self.view.frame.size.height - (self.view.frame.size.height - 80),
+                                   160,
+                                   36.0f);
+    self.RSSchoolLabel = [[UILabel alloc] initWithFrame:labelFrame];
+    self.RSSchoolLabel.text = @"RSSchool";
+    self.RSSchoolLabel.numberOfLines = 1;
+    self.RSSchoolLabel.font = [UIFont boldSystemFontOfSize:36.0f];
+    // add SF pro
+    self.RSSchoolLabel.center = CGPointMake(self.view.center.x, self.view.frame.size.height - (self.view.frame.size.height - 80));
+    self.RSSchoolLabel.textColor = UIColor.blackColor;
+    [self.view addSubview:self.RSSchoolLabel];
 }
 @end
